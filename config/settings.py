9e8c3880c7b227077de762189a43d01ce0100786
= <sohyeonpark@koreanair.com>
2024-01-18 00:58:21 +0900

    update

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(g(6$8jiv9@6@(8(ae=6lejjy+q4@)9c^s7^^nri01jyb4inh^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['43.200.235.128']
 
 
 # Application definition
--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ from pybo.views import base_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
+    path('ensemble/', include('ensemble.urls')),
     path('common/', include('common.urls')),
     path('', base_views.index, name='index'),  # '/' 에 해당되는 path
 ]
